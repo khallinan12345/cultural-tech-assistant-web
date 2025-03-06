98b0c3e2cac08114f06881ebb13df41f929a3d4d
khallinan12345 <kevin.hallinan.ud@gmail.com>
2025-03-06 09:32:51 -0500

    Configure Vite app for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cultural-appropriate-technology-trainer/', // Your repo name
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  }
-});
+  base: '/cultural-tech-assistant-web/', // Add this line for GitHub Pages
+})
